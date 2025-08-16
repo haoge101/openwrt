6622fbeac37f2d60ae27b2388090201597e6ca82
haoge101 <66171250+haoge101@users.noreply.github.com>
2025-08-16 18:11:48 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,3 +17,8 @@
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git openlist https://github.com/sbwml/luci-app-openlist2' >>feeds.conf.default
+echo 'src-git luci-app-syncthing https://github.com/danchexiaoyang/luci-app-syncthing' >>feeds.conf.default
+echo 'src-git luci-app-diskman https://github.com/lisaac/luci-app-diskman' >>feeds.conf.default
+echo 'src-git luci-theme-argon https://github.com/jerrykuku/luci-theme-argon' >>feeds.conf.default
+
