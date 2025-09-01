d19f0d7786ab09fe6ce1f6ca95a1185715a8a436
haoge101 <66171250+haoge101@users.noreply.github.com>
2025-09-01 19:29:34 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -37,8 +37,24 @@ rm -rf feeds/luci/applications/luci-app-diskman
 git clone https://github.com/lisaac/luci-app-diskman package/luci-app-diskman
 
 # 更新openList2版本
-
 git clone https://github.com/sbwml/luci-app-openlist2 package/openlist
+
+# 更新Syncthing
+# 1. 获取 Syncthing 最新版本号
+LATEST=$(curl -s https://api.github.com/repos/syncthing/syncthing/releases/latest \
+  | grep '"tag_name":' \
+  | sed -E 's/.*"v?([^"]+)".*/\1/')
+
+# 2. OpenWrt syncthing Makefile 路径
+MAKEFILE="feeds/packages/utils/syncthing/Makefile"
+
+# 3. 替换 Makefile 中的 PKG_VERSION
+sed -i -E "s/^(PKG_VERSION:)=.*/\1 $LATEST/" "$MAKEFILE"
+
+# 4. 显示PKG_VERSION
+# 提取 version
+VERSION=$(grep -E '^PKG_VERSION:=' "$MAKEFILE")
+echo "Current version in Syncthing Makefile: $VERSION"
 
 
 
