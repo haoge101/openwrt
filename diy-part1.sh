02501eb8b5f19a390edc7b79a6ea8dc96cc388f9
haoge101 <66171250+haoge101@users.noreply.github.com>
2025-09-01 09:27:44 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,3 +17,22 @@
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
+# luci-app-passwall
+git clone -b main https://github.com/xiaorouji/openwrt-passwall.git package/passwall
+git clone -b main https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall-packages
+
+# luci-app-diskman
+mkdir -p package/luci-app-diskman && \
+wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/applications/luci-app-diskman/Makefile -O package/luci-app-diskman/Makefile
+mkdir -p package/parted && \
+wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/Parted.Makefile -O package/parted/Makefile
+
+# luci-app-openlist2
+git clone https://github.com/sbwml/luci-app-openlist2 package/openlist
+
+# luci-app-wechatpush
+git clone -b master https://github.com/tty228/luci-app-wechatpush.git package/luci-app-wechatpush
+
+
+
+
