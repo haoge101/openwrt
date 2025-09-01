fbf7feefa804c7ce32fe2939395c82e7dfd5f105
haoge101 <66171250+haoge101@users.noreply.github.com>
2025-09-01 21:10:16 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -45,19 +45,10 @@ git clone https://github.com/tty228/luci-app-wechatpush package/luci-app-wechatp
 
 
 # 更新Syncthing
-# 1. 获取 Syncthing 最新版本号
-LATEST=$(curl -s https://api.github.com/repos/syncthing/syncthing/releases/latest | grep '"tag_name":' | sed -E 's/.*"v?([^"]+)".*/\1/')
-
-# 2. OpenWrt syncthing Makefile 路径
-MAKEFILE="feeds/packages/utils/syncthing/Makefile"
-
-# 3. 替换 Makefile 中的 PKG_VERSION
-sed -i -E "s/^(PKG_VERSION:)=.*/\1 $LATEST/" "$MAKEFILE"
-
-# 4. 显示PKG_VERSION
-# 提取 version
-VERSION=$(grep -E '^PKG_VERSION:=' "$MAKEFILE")
-echo "Current version in Syncthing Makefile: $VERSION"
+LATEST=$(curl -s https://api.github.com/repos/syncthing/syncthing/releases/latest | jq -r '.tag_name' | sed 's/^v//')
+echo "Latest version: $LATEST"
+sed -i -E "s/^[[:space:]]*PKG_VERSION:=.*/PKG_VERSION:=$LATEST/" feeds/packages/utils/syncthing/Makefile
+grep PKG_VERSION feeds/packages/utils/syncthing/Makefile
 
 
 
